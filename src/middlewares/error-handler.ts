74456280443fee9a7c7fbc37b29fba9603330d1f
wgbg <kamil.szela.dev@gmail.com>
2020-11-25 14:49:09 +0100

    update error handler middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,16 +1,20 @@
-import { StatusCodes } from 'http-status-codes'
-
 export const errorHandlerMiddleware = (debug?: boolean) => {
   return (err, req, res, next) => {
-    const statusCode = err.statusCode || 500
+    const statusCode = err.statusCode || err.status || 500
+
+    if (debug) {
+      console.log(err)
+    }
+
     res.status(statusCode).json({
-      error: {
-        code: err.code || err.statusCode,
+      errors: err.errors,
+      meta: {
+        code: err.code || err.statusCode || err.status,
         debug: (debug) ? {
           debug: err.debug,
-          err: err
-        } : undefined,
-        message: (err.code || statusCode !== StatusCodes.INTERNAL_SERVER_ERROR) ? undefined : err.message
+          err: err.toString(),
+          errObj: JSON.stringify(err)
+        } : undefined
       }
     })
   }

d4b8a6a767654ba75f2224f27d34caf38a564207
wgbg <kamil.szela.dev@gmail.com>
2020-11-25 14:58:20 +0100

    refactor test-cases simpleCrud usage

--- src/functionalities/test-cases/api.ts
+++ src/functionalities/test-cases/api.ts
@@ -3,34 +3,41 @@ import { testCasesRepository } from '../../repositories'
 import { SimpleCrud } from '../../common'
 import { testCaseTransformer } from '../../entity-transformers'
 import { EntitiesNames } from '../../database'
+import { NextFunction } from 'express'
 
-export const createTestCaseApi = async (req:Api.CreateTestCase.ApiRequest, res: Api.CreateTestCase.ApiResponse) => {
+export const createTestCaseApi = async (
+  req:Api.CreateTestCase.ApiRequest,
+  res: Api.CreateTestCase.ApiResponse,
+  next: NextFunction
+) => {
   return SimpleCrud.simpleCreate({
     createCallback: testCasesRepository.bindCreate(),
     transformer: testCaseTransformer,
-    entityName: EntitiesNames.TestCase,
-    req,
-    res
-  })
+    entityName: EntitiesNames.TestCase
+  })(req, res, next)
 }
 
-export const getTestCaseApi = async (req:Api.GetTestCase.ApiRequest, res: Api.GetTestCase.ApiResponse) => {
+export const getTestCaseApi = async (
+  req:Api.GetTestCase.ApiRequest,
+  res: Api.GetTestCase.ApiResponse,
+  next: NextFunction
+) => {
   return SimpleCrud.simpleGet({
     findEntityCallback: testCasesRepository.bindFindById(),
     transformerCallback: testCaseTransformer,
-    entityName: EntitiesNames.TestCase,
-    req,
-    res
-  })
+    entityName: EntitiesNames.TestCase
+  })(req, res, next)
 }
 
-export const updateTestCaseApi = async (req:Api.UpdateTestCases.ApiRequest, res: Api.UpdateTestCases.ApiResponse) => {
+export const updateTestCaseApi = async (
+  req:Api.UpdateTestCases.ApiRequest,
+  res: Api.UpdateTestCases.ApiResponse,
+  next: NextFunction
+) => {
   return SimpleCrud.simpleUpdate({
     findEntityCallback: testCasesRepository.bindFindById(),
     updateEntityCallback: testCasesRepository.bindUpdate(),
     transformerCallback: testCaseTransformer,
-    entityName: EntitiesNames.TestCase,
-    req,
-    res
-  })
+    entityName: EntitiesNames.TestCase
+  })(req, res, next)
 }
--- tests/unit/functionalities/test-cases/api.spec.ts
+++ tests/unit/functionalities/test-cases/api.spec.ts
@@ -1,21 +1,11 @@
 
-import { createTestCaseApi, getTestCaseApi, updateTestCaseApi } from '../../../../src/functionalities/test-cases/api'
 import testCaseFunc from '../../../../src/functionalities/test-cases'
 import { apiV1Route } from '../../../../src/common/routes'
-import { testRoutes, testSimpleCrudRoutes } from '../api-common'
-import { SimpleCrud } from '../../../../src/common'
-jest.mock('../../../../src/common/simple-crud')
+import { testRoutes } from '../api-common'
 
 describe('functionalities', () => {
   describe('test-cases', () => {
     describe('api', () => {
-      it('all pi', async () => {
-        await testSimpleCrudRoutes([
-          createTestCaseApi,
-          getTestCaseApi,
-          updateTestCaseApi
-        ])
-      })
       it('Routes', () => {
         const requiredRoutes = [
           {

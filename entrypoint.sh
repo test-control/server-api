9306e465cdd4e0a11125ccb6dd436ca64f78bd34
wgbg <kamil.szela.dev@gmail.com>
2020-12-08 19:59:19 +0100

    prepare deps for multi databases

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# This is version of local development entrypoint
+
+npm run check-system || exit 1
+
 npm run db:migrate:latest || exit 1
+
+npm run db:seed || exit 1
 
 npm run dev

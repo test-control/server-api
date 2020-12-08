50b892d9f84cfce049925e6109423d6af8ad83ad
wgbg <kamil.szela.dev@gmail.com>
2020-12-08 21:06:38 +0100

    configure db connection for mysql, postgresql, mssql

---
+++ src/auto-types/schemas/databaseEngines.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+export interface DatabaseEngines {
+  types: Types;
+}
+
+export const enum Types {
+  postgresql = 'postgresql',
+  mysql = 'mysql'
+}
+
--- src/database/connections.ts
+++ src/database/connections.ts
@@ -4,14 +4,38 @@ const { attachPaginate } = require('knex-paginate')
 
 let appConnection : Knex
 
-export const getAppConnection = () : Knex => {
-  if (!appConnection) {
-    appConnection = Knex({
-      client: 'pg',
-      connection: getEnvs().POSTGRESQL_CONNECTION_STRING,
+const dbSettingsMap = {
+  postgresql: {
+    driver: 'pg',
+    env: 'POSTGRESQL_CONNECTION_STRING',
+    extraParams: {
       searchPath: ['public']
-    })
+    }
+  },
+  mysql: {
+    driver: 'mysql',
+    env: 'MYSQL_CONNECTION_STRING',
+    extraParams: {}
+  },
+  mssql: {
+    driver: 'mssql',
+    env: 'MSSQL_CONNECTION_STRING',
+    extraParams: {}
   }
+}
+
+export const getAppConnection = () : Knex => {
+  if (appConnection) {
+    return appConnection
+  }
+
+  const settings = dbSettingsMap[getEnvs().DATABASE_ENGINE]
+
+  appConnection = Knex({
+    client: settings.driver,
+    connection: getEnvs()[settings.env],
+    ...settings.extraParams
+  })
 
   return appConnection
 }

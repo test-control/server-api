eba536b7381b14b2003c9879466ea25af32925fe
wgbg <kamil.szela.dev@gmail.com>
2021-01-23 18:57:36 +0100

    internalerror when cannot find tree

--- src/functionalities/projects/events-listeners.ts
+++ src/functionalities/projects/events-listeners.ts
@@ -1,5 +1,5 @@
 import { projectTreesRepository, treesRepository } from '../../repositories'
-import { EntityEvent } from '../../common'
+import { EntityEvent, InternalError } from '../../common'
 import { Schemas } from '../../auto-types'
 import moment from 'moment'
 
@@ -8,6 +8,13 @@ export const createTreeRoot = async (event: EntityEvent<Schemas.Entities.Project
     title: 'root',
     created_at: moment().format()
   })
+
+  if (!tree) {
+    throw new InternalError({
+      info: 'cannot create tree root',
+      event: event
+    })
+  }
 
   return projectTreesRepository.addProjectTree(
     event.entity.id,

b97f43fdd5e01c3d2cbe941a0b5b63fa7f3767b7
wgbg <kamil.szela.dev@gmail.com>
2021-02-04 22:39:51 +0100

    create helper for generating all leaves from root to given path

--- src/common/trees.ts
+++ src/common/trees.ts
@@ -9,3 +9,29 @@ export const extractRootFromPath = (path: string) : string | null => {
 
   return found[1]
 }
+
+export const getAllLeavesFromRoot = (path: string) : string[] | null => {
+  const found = path.match(/^([0-9]{1,}\.?)+$/)
+
+  if (!found || typeof found[0] === 'undefined') {
+    return null
+  }
+
+  const parts = path.split('.')
+  const leaves = []
+
+  var lastLeaf = ''
+
+  for (var part of parts) {
+    if (!lastLeaf) {
+      leaves.push(part)
+      lastLeaf = part
+      continue
+    }
+
+    lastLeaf = lastLeaf + '.' + part
+    leaves.push(lastLeaf)
+  }
+
+  return leaves
+}

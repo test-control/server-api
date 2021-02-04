a33a8b888e050b88788643cf6b2aad6f6434a8fc
wgbg <kamil.szela.dev@gmail.com>
2021-02-04 22:38:36 +0100

    new api treeRootPath

---
+++ src/auto-types/api/get-tree-root-path.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+import { Request, Response } from 'express'
+
+export namespace GetTreeRootPath{
+  export interface PathRequestParams {
+    entityId: string;
+  }
+  export interface QueryRequestParams {}
+  export interface CookieRequestParams {}
+  export interface HeaderRequestParams {}
+  export type ApiRequest = Request<Required<PathRequestParams>, ResponseBody, {}, QueryRequestParams>
+  export interface ApplicationJson200ResponseBody {
+    data: {
+      /**
+       * Unique id
+       */
+      id: string;
+      /**
+       * Title of root leaf
+       */
+      title: string;
+      createdAt: string;
+    }[];
+  }
+  export type ResponseBody = ApplicationJson200ResponseBody
+  export type ApiResponse = Response<ResponseBody>
+}

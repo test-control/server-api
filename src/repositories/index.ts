1be6e8b8aecde09ed2dfcbd915cc05cb76238e85
wgbg <kamil.szela.dev@gmail.com>
2021-03-06 15:14:00 +0100

    use different repositories for test-cases

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,13 +1,14 @@
 import { getAppConnection } from '../database'
 import { ProjectTreesRepository } from './project-trees'
 import { ProjectsRepository } from './projects'
-import { TestCasesRepository } from './test-cases'
 import { TestCasesPreconditionsRepository } from './test-cases-preconditions'
 import { TestCasesStepsRepository } from './test-cases-steps'
 import { DATABASE_ENGINE } from '../auto-types/schemas'
 import { TreesRepository } from './trees'
 import { MysqlTreesRepository } from './mysql'
 import { MssqlTreesRepository } from './mssql/trees'
+import { TestCasesRepository } from './test-cases'
+import { MssqlTestCasesRepository } from './mssql/test-cases'
 
 interface InstantiateRepository<T>{
   isDefault?: boolean;
@@ -36,7 +37,18 @@ const instantiateRepository = <T>(vars: Array<InstantiateRepository<T>>) : T => 
 
 export const projectsRepository = new ProjectsRepository(getAppConnection)
 export const projectTreesRepository = new ProjectTreesRepository(getAppConnection)
-export const testCasesRepository = new TestCasesRepository(getAppConnection)
+export const testCasesRepository = instantiateRepository([
+  {
+    isDefault: true,
+    engine: DATABASE_ENGINE.postgresql,
+    repository: () => new TestCasesRepository(getAppConnection)
+  },
+  {
+    isDefault: false,
+    engine: DATABASE_ENGINE.mssql,
+    repository: () => new MssqlTestCasesRepository(getAppConnection)
+  }
+])
 export const testCasesPreconditionsRepository = new TestCasesPreconditionsRepository(getAppConnection)
 export const testCasesStepsRepository = new TestCasesStepsRepository(getAppConnection)
 export const treesRepository = instantiateRepository([

ffb3b2a4f8c7cd118f98240dedc1528fd464eec1
wgbg <kamil.szela.dev@gmail.com>
2021-08-19 22:00:02 +0200

    bump knex version and use ts config file, install passport and crypt

--- db/knexfile.ts
+++ db/knexfile.ts
@@ -1,24 +1,31 @@
-module.exports = {
+import { setEnvsSettings } from '../src/settings'
+
+setEnvsSettings()
+
+export default {
   postgresql: {
     client: 'pg',
     connection: process.env.POSTGRESQL_CONNECTION_STRING,
     searchPath: ['public'],
     seeds: {
-      directory: './seeds'
+      directory: './seeds',
+      loadExtensions: ['.js', '.ts']
     }
   },
   mysql: {
     client: 'mysql',
     connection: process.env.MYSQL_CONNECTION_STRING,
     seeds: {
-      directory: './seeds'
+      directory: './seeds',
+      loadExtensions: ['.js', '.ts']
     }
   },
   mssql: {
     client: 'mssql',
     connection: process.env.MSSQL_CONNECTION_STRING,
     seeds: {
-      directory: './seeds'
+      directory: './seeds',
+      loadExtensions: ['.js', '.ts']
     }
   }
 }
